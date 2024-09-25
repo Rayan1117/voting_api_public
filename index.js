a217ad7506c8450297c8e01b3dd426416e3da09e
Shiranjan <shiranjansanju@gmail.com>
2024-09-25 15:22:44 +0530

    init

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-const express =require('express')
-const app=express()
+const express = require('express')
+const app = express()
+const sql = require('mssql')
 
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+const pins = [1, 2, 3, 5, 6, 7, 8, 4];
+
+app.use(express.json());
+
+dbConfig = {
+    server: 'MZCETDB',
+    database: 'CSE8761',
+    user: 'MZCET',
+    password: 'MZCET@1234',
+    options: {
+        trustServerCertificate: true
+    }
+}
+
+app.post("/config", async (req, res) => {
+    try {
+        const pool = await sql.connect(dbConfig);
+        const request = new sql.Request;
+        const { id } = req.body;
+        const { pins } = req.body;
+        const query = 'INSERT INTO config (id,pins) VALUES (@id,@pins)';
+
+        await request.input('id', sql.VarChar, id).input('pins', sql.NVarChar, pins).query(query);
+
+        res.send(pins);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send("FAILED");
+
+    }
+});
+
+app.get("/test", (req, res) => {
+    res.send(pins);
+});
+
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
