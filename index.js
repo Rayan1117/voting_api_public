684a1a31b33e90e7c39c0293e3327ab44d71665d
Lakshmanan <lakshmanan8836@mountzion.ac.in>
2024-09-30 13:22:17 +0530

    change1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const sql = require('mssql')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ||5000;
 
 const pins = [1, 2, 3, 5, 6, 7, 8, 4];
 
